0ad639d4ca77acc39075eaf43d9fbfce64db6c74
Beatriz Reis <32250392+BeatrizReisDutra@users.noreply.github.com>
2025-04-01 20:07:34 -0400

    Criado usando o Colab

---
+++ Aula1Web2.py
@@ -0,0 +1,48 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNCtYvXKCuyeeMRJA0mVsQn",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/BeatrizReisDutra/AulaProgWebBD/blob/master/Aula1Web2.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "dzcC6K1SzRcT"
+      },
+      "outputs": [],
+      "source": [
+        "class Notebook:\n",
+        "  def __init__(self, marca, polegada, processador, memoria, armazenamento, preco):\n",
+        "    self.marca = marca\n",
+        "    self.polegada = polegada\n",
+        "    self.processador = processador\n",
+        "    self.memoria = memoria\n",
+        "    self.armazenamento = armazenamento\n",
+        "    self.preco = preco\n"
+      ]
+    }
+  ]
+}
